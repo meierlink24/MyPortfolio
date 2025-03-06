83052c075283e3af00fc9ce472fc5aeb88ff6f4d
Meier <meier.link24@gmail.com>
2025-03-06 22:35:21 +0000

    nav

---
+++ portfolio/src/components/App.js
@@ -0,0 +1,12 @@
+
+import '/workspaces/MyPortfolio/portfolio/src/style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Nav.js
@@ -0,0 +1,12 @@
+import '/workspaces/MyPortfolio/portfolio/src/style/Components.css'
+
+export const NavMenu = () => {
+    return (
+        <div classNme="NavBar">
+            <a href="#">Home</a>
+            <a href="#">Skils</a>
+            <a href="#">Projects</a>
+            <a href="#">Contact</a>
+        </div>
+    );
+};
